0485ee23440c20e04ecdae6b19b6c03772faadc8
mypapi9160 <158793809+mypapi9160@users.noreply.github.com>
2024-03-24 13:24:26 +0900

    kadai_005を再アップロード

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,8 +1,8 @@
 //体重　定数の宣言・値の代入
-const bodyWeight = 68;
+let bodyWeight = 68;
 
 //身長　定数の宣言・値の代入
-const bodyHeight = 1.7;
+let bodyHeight = 1.7;
 
 //コンソールへの出力
 console.log(bodyWeight /( bodyHeight * bodyHeight));
